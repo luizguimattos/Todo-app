dba963b439d34699989929cf699525a4bdc98d34
Luiz Guilherme Lima de Mattos <l.g.mattos@hotmail.com>
2024-05-15 09:03:40 -0300

    Change icon

--- src/components/EmptyList/EmptyList.tsx
+++ src/components/EmptyList/EmptyList.tsx
@@ -1,4 +1,4 @@
-import { Tray } from '@phosphor-icons/react';
+import { CheckFat } from '@phosphor-icons/react';
 import styles from './EmptyList.module.css'
 
 export function EmptyList() {
@@ -6,7 +6,7 @@ export function EmptyList() {
     return (
         <div className={styles.container}>
 
-            <Tray size={32} />
+            <CheckFat size={32} />
 
             <h4>Você não possui todo item cadastrado.</h4>
         </div>
